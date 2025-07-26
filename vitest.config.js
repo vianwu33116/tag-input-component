99481951ed02f50db48a9853bd8e66abf36820c5
vianwu33116 <wserena33116@gmail.com>
2025-07-26 12:20:43 +0800

    remove unnecessary comment and exclude **/*.test.js

--- vitest.config.js
+++ vitest.config.js
@@ -9,17 +9,9 @@ export default defineConfig({
       provider: "v8",
       reporter: ["text", "json", "html"],
       include: ["**/*.test.js"],
-      exclude: ["node_modules/", "__tests__/setup.js", "**/*.test.js"],
+      exclude: ["node_modules/", "__tests__/setup.js"],
     },
     testTimeout: 10000,
     hookTimeout: 10000,
   },
 });
-
-// import { defineConfig } from 'vite';
-
-// export default defineConfig({
-//   test: {
-//     environment: 'jsdom',
-//   },
-// });
